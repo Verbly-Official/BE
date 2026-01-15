e1e9a3278a4d37e6213ca7f67ab1e5e1c0c521ab
tellgeniewish <jadastras2@gmail.com>
2026-01-16 03:17:24 +0900

    [Feat] 인증/인가 예외 처리 핸들러 추가

---
+++ src/main/java/verbly/spring/global/security/handler/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,36 @@
+package verbly.spring.global.security.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+import verbly.spring.global.common.code.ErrorStatus;
+import verbly.spring.global.common.dto.ErrorReasonDTO;
+import verbly.spring.global.common.response.ApiResponse;
+
+import java.io.IOException;
+
+@Component
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint { // 인증 실패 시 401 에러를 반환
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException {
+
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json;charset=UTF-8");
+
+        ErrorReasonDTO reason = ErrorReasonDTO.builder()
+                .httpStatus(ErrorStatus.INVALID_JWT_ACCESS_TOKEN.getHttpStatus())
+                .isSuccess(false)
+                .code(ErrorStatus.INVALID_JWT_ACCESS_TOKEN.getCode())
+                .message(ErrorStatus.INVALID_JWT_ACCESS_TOKEN.getMessage())
+                .build();
+
+        response.getWriter().write(new ObjectMapper().writeValueAsString(
+                ApiResponse.onFailure(reason.getCode(), reason.getMessage(), reason)
+        ));
+    }
+}

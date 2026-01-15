ba01f12783cfabc70738b643ba0345f6ec725ff2
tellgeniewish <jadastras2@gmail.com>
2026-01-16 04:09:05 +0900

    [Feat] OAuth2UserInfo 추가

---
+++ src/main/java/verbly/spring/global/security/oauth/userinfo/OAuth2UserInfo.java
@@ -0,0 +1,16 @@
+package verbly.spring.global.security.oauth.userinfo;
+
+import lombok.Getter;
+
+import java.util.Map;
+
+@Getter
+public abstract class OAuth2UserInfo {
+    protected Map<String, Object> attributes;
+
+    public OAuth2UserInfo(Map<String, Object> attributes) {
+        this.attributes = attributes;
+    }
+
+    public abstract String getSocialId();
+}

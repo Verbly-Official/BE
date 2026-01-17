ca93213e8095a40eb200a78254bde7de92412cdc
tellgeniewish <jadastras2@gmail.com>
2026-01-18 05:18:25 +0900

    [Fix] Amazon Config가 prod일 때 동작하도록

--- src/main/java/verbly/spring/global/config/AmazonConfig.java
+++ src/main/java/verbly/spring/global/config/AmazonConfig.java
@@ -11,7 +11,9 @@ import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
+@Profile("prod")
 @Configuration
 @Getter
 public class AmazonConfig {

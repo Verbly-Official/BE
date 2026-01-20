50eb935bb29394cd45fb19d2075aa921dcf5d3a8
tellgeniewish <jadastras2@gmail.com>
2026-01-21 00:36:11 +0900

    [Feat] UserCommandService 추가

--- src/main/java/verbly/spring/domain/user/controller/UserRestController.java
+++ src/main/java/verbly/spring/domain/user/controller/UserRestController.java
@@ -14,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 import verbly.spring.domain.user.dto.request.UserRequestDTO;
 import verbly.spring.domain.user.dto.response.UserResponseDTO;
 import verbly.spring.domain.user.entity.User;
+import verbly.spring.domain.user.service.UserCommandService;
 import verbly.spring.domain.user.service.UserQueryService;
 import verbly.spring.global.common.code.SuccessStatus;
 import verbly.spring.global.common.response.ApiResponse;

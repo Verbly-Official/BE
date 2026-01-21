de990df3d31378a9c59fa1040c6b8cd7d4820dd1
tellgeniewish <jadastras2@gmail.com>
2026-01-22 01:37:27 +0900

    [Feat] StatsCommandServiceImpl 구현체 추가

--- src/main/java/verbly/spring/global/common/code/ErrorStatus.java
+++ src/main/java/verbly/spring/global/common/code/ErrorStatus.java
@@ -29,7 +29,9 @@ public enum ErrorStatus implements BaseErrorCode {
     INVALID_JWT_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "TOKEN4001", "유효하지 않은 AccessToken입니다."),
     INVALID_JWT_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "TOKEN4002", "유효하지 않은 RefreshToken입니다."),
     INVALID_SOCIAL_TOKEN(HttpStatus.UNAUTHORIZED, "TOKEN4003", "유효하지 않은 SocialToken입니다."),
-    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "PW4001", "잘못된 비밀번호입니다.");
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "PW4001", "잘못된 비밀번호입니다."),
+
+    STATS_NOT_FOUND();
 
     private final HttpStatus httpStatus;
     private final String code;

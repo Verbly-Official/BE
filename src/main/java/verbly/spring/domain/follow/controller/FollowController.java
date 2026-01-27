569a50e52966384934b0ae10be15a4b1503a89f2
Ddiy0ng <bueong0627@gmail.com>
2026-01-27 18:08:31 +0900

    [Fix] 리뷰 api 및 swagger 수정 #16
    리뷰 평균은 소수점 첫째 자리, 상위 백분율은 정수 까지 표현
    
    소셜 회원가입 후 stats 초기화에 대해 review 관련 속성 0으로 할당

--- src/main/java/verbly/spring/domain/follow/controller/FollowController.java
+++ src/main/java/verbly/spring/domain/follow/controller/FollowController.java
@@ -3,12 +3,8 @@ package verbly.spring.domain.follow.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.Parameters;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.constraints.Null;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -24,7 +20,7 @@ import java.util.List;
 @Tag(name = "Follow", description = "팔로우/팔로잉 API")
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api/following")
+@RequestMapping("/api/following")
 public class FollowController {
 
     private final FollowService followService;
@@ -32,19 +28,16 @@ public class FollowController {
     @Operation(
             summary = "팔로우 생성",
             security = @SecurityRequirement(name = "JWT TOKEN"),
-            description = "특정 사용자를 팔로우합니다.\n\n" +
-                    "### PathVariable\n" +
-                    "- followeeId: 팔로우 대상 사용자 ID\n\n" +
-                    "### 실패 케이스 예시\n" +
-                    "- 자기 자신 팔로우 시도\n" +
-                    "- 이미 팔로우한 사용자\n" +
-                    "- 대상 사용자 없음"
+            description =
+                    "특정 사용자를 팔로우합니다.\n\n" +
+                    "- 스스로 팔로우 불가" +
+                    "- 중복 팔로우 불가"
     )
-    @Parameters({
-            @Parameter(name = "followeeId", description = "팔로우 대상 사용자 ID", example = "1")
-    })
     @PostMapping("/{followeeId}")
-    public ResponseEntity<ApiResponse<Void>> createFollowing(@AuthenticationPrincipal CustomUserDetails customUserDetails, @PathVariable Long followeeId) {
+    public ResponseEntity<ApiResponse<Void>> createFollowing(
+            @AuthenticationPrincipal CustomUserDetails customUserDetails,
+            @Parameter(name = "followeeId", description = "팔로우 대상 사용자 ID", example = "1")
+            @PathVariable Long followeeId) {
 
         Long followerId = customUserDetails.getUser().getId();
         followService.createFollowing(followerId, followeeId);
@@ -57,9 +50,9 @@ public class FollowController {
     @Operation(
             summary = "추천 팔로우 목록 조회",
             security = @SecurityRequirement(name = "JWT TOKEN"),
-            description = "팔로우 추천 사용자 목록을 조회합니다.\n\n" +
-                    "### 반환\n" +
-                    "- 랜덤 추천 사용자 리스트 (현재 서버 로직: 최대 3명)\n"
+            description =
+                    "팔로우 추천 사용자 목록을 조회합니다.\n\n" +
+                    "- 랜덤 추천 사용자 리스트 (3명)\n"
     )
     @GetMapping("/recommend")
     public ResponseEntity<ApiResponse<List<UserResponseDTO.FollowRecommendUserResponseDTO>>> getRecommendFollowList(@AuthenticationPrincipal CustomUserDetails customUserDetails) {
@@ -72,19 +65,15 @@ public class FollowController {
     }
 
     @Operation(
-            summary = "언팔로우",
+            summary = "언팔로우(팔로우 취소)",
             security = @SecurityRequirement(name = "JWT TOKEN"),
-            description = "특정 사용자를 언팔로우합니다.\n\n" +
-                    "### PathVariable\n" +
-                    "- followeeId: 언팔로우 대상 사용자 ID\n\n" +
-                    "### 실패 케이스 예시\n" +
-                    "- 팔로우 관계가 없는 사용자 언팔로우 시도"
+            description = "특정 사용자를 언팔로우합니다.\n\n"
     )
-    @Parameters({
-            @Parameter(name = "followeeId", description = "언팔로우 대상 사용자 ID", example = "1")
-    })
     @DeleteMapping("/{followeeId}")
-    public ResponseEntity<ApiResponse<Void>> unfollow(@AuthenticationPrincipal CustomUserDetails customUserDetails, @PathVariable Long followeeId) {
+    public ResponseEntity<ApiResponse<Void>> unfollow(
+            @AuthenticationPrincipal CustomUserDetails customUserDetails,
+            @Parameter(name = "followeeId", description = "언팔로우 대상 사용자 ID", example = "1")
+            @PathVariable Long followeeId) {
 
         Long followerId = customUserDetails.getUser().getId();
         followService.unfollow(followerId, followeeId);
--- src/main/java/verbly/spring/domain/stats/repository/StatsRepository.java
+++ src/main/java/verbly/spring/domain/stats/repository/StatsRepository.java
@@ -1,8 +1,8 @@
 package verbly.spring.domain.stats.repository;
 
-import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import verbly.spring.domain.stats.entity.Stats;
 
 import java.util.Optional;
@@ -11,7 +11,7 @@ public interface StatsRepository extends JpaRepository<Stats, Long> {
     Optional<Stats> findByUserId(Long userId);
 
     @Query(value = """
-        SELECT ROUND(AVG(r.rating))
+        SELECT (ROUND(AVG(r.rating), 1))
         FROM Review r
         WHERE r.reviewee.id = :revieweeId
     """
@@ -20,10 +20,10 @@ public interface StatsRepository extends JpaRepository<Stats, Long> {
 
     // reviewCount 0인 경우도 포함
     @Query(value = """
-        SELECT(reviewee_ranking_list.rank * 100.0 / reviewee_ranking_list.reviewee_amount)
+        SELECT(ROUND((reviewee_ranking_list.ranking * 100.0 / reviewee_ranking_list.reviewee_amount)))
         FROM(
             SELECT stats.user_id reviewee_id,
-                RANK() OVER(ORDER BY COALESCE(stats.review_average, 0) DESC) AS rank,
+                RANK() OVER(ORDER BY COALESCE(stats.review_average, 0) DESC) AS ranking,
                 COUNT(*) OVER () AS reviewee_amount
             FROM stats stats) reviewee_ranking_list
        WHERE reviewee_ranking_list.reviewee_id = :revieweeId

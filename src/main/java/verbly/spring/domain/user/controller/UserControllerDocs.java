79e93b5e7923410b196f147334159202aa41e4da
keypang <bestnest820@naver.com>
2026-02-05 03:18:33 +0900

    [Feat] API 문서화

--- src/main/java/verbly/spring/domain/user/controller/UserControllerDocs.java
+++ src/main/java/verbly/spring/domain/user/controller/UserControllerDocs.java
@@ -1,5 +1,11 @@
 package verbly.spring.domain.user.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
 import verbly.spring.domain.user.dto.response.UserResponseDTO;
@@ -8,12 +14,80 @@ import verbly.spring.global.security.auth.CustomUserDetails;
 
 import java.util.UUID;
 
+@Tag(name = "Home-User", description = "홈 화면 유저 관련 API")
 public interface UserControllerDocs {
 
+    @Operation(
+            summary = "홈 화면 유저 정보 조회",
+            security = @SecurityRequirement(name = "JWT TOKEN")
+    )
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    responseCode = "200",
+                    description = "성공",
+                    content = @Content(
+                            mediaType = "application/json",
+                            examples = @ExampleObject(
+                                    value = """
+                                            {
+                                                "isSuccess": true,
+                                                "code": "COMMON2000",
+                                                "message": "성공입니다.",
+                                                "result": {
+                                                    "imageUrl": "http://...",
+                                                    "nickname": "박시윤",
+                                                    "following": 0,
+                                                    "streak": 0,
+                                                    "point": 100,
+                                                    "correctionReceived": 0,
+                                                    "level": "LV1"
+                                                }
+                                            }
+                                        """
+                            )
+                    )
+            )
+    })
     ApiResponse<UserResponseDTO.HomeViewerInfoDTO> getHomeViewerInfo(
             @AuthenticationPrincipal CustomUserDetails userDetails
     );
 
+    @Operation(
+            summary = "특정 유저 프로필 확인 API",
+            security = @SecurityRequirement(name = "JWT TOKEN"),
+            description = "✅ **path variable:**\n" +
+                          "- uuid: 상대 유저 uuid (UUID)\n"
+    )
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    responseCode = "200",
+                    description = "성공",
+                    content = @Content(
+                            mediaType = "application/json",
+                            examples = @ExampleObject(
+                                    value = """
+                                            {
+                                                "isSuccess": true,
+                                                "code": "COMMON2000",
+                                                "message": "성공입니다.",
+                                                "result": {
+                                                    "imageUrl": "http://...",
+                                                    "nickname": "aa",
+                                                    "nativeLang": "KO",
+                                                    "description": "asdf",
+                                                    "totalPosts": 2,
+                                                    "follower": 0,
+                                                    "following": 1,
+                                                    "isFollowing": false,
+                                                    "correctionReceived": 0,
+                                                    "correctionGiven": 0
+                                                }
+                                            }
+                                        """
+                            )
+                    )
+            )
+    })
     ApiResponse<UserResponseDTO.HomeUserInfoDTO> getUserProfileInfo(
             @AuthenticationPrincipal CustomUserDetails userDetails,
             @PathVariable(name = "uuid") UUID uuid

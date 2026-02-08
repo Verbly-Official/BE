4f3dfb819e134148407a0970717c92618663b205
keypang <bestnest820@naver.com>
2026-02-08 23:37:25 +0900

    [Feat] 전체 읽음 처리

--- src/main/java/verbly/spring/domain/notification/controller/NotificationRestController.java
+++ src/main/java/verbly/spring/domain/notification/controller/NotificationRestController.java
@@ -6,10 +6,12 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import verbly.spring.domain.notification.dto.response.NotificationsResponseDTO;
+import verbly.spring.domain.notification.service.NotificationCommandService;
 import verbly.spring.domain.notification.service.NotificationQueryService;
 import verbly.spring.domain.notification.service.NotificationService;
 import verbly.spring.global.common.response.ApiResponse;
@@ -24,6 +26,7 @@ public class NotificationRestController {
 
     private final NotificationService notificationService;
     private final NotificationQueryService notificationQueryService;
+    private final NotificationCommandService notificationCommandService;
 
     @GetMapping(value = "/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public SseEmitter subscribe() {
@@ -37,5 +40,13 @@ public class NotificationRestController {
     ) {
         Long userId = SecurityUtils.getCurrentUserId();
         return ApiResponse.onSuccess(notificationQueryService.getNotifications(userId, pageable));
+    }
+
+    @PatchMapping()
+    public ApiResponse<List<NotificationsResponseDTO.NotificationDTO>> patchNotifications(
+            @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable
+    ) {
+        Long userId = SecurityUtils.getCurrentUserId();
+        return ApiResponse.onSuccess(notificationCommandService.patchNotifications(userId, pageable));
     }
 }
--- src/main/java/verbly/spring/domain/notification/service/NotificationCommandService.java
+++ src/main/java/verbly/spring/domain/notification/service/NotificationCommandService.java
@@ -1,5 +1,10 @@
 package verbly.spring.domain.notification.service;
 
-public interface NotificationCommandService {
+import org.springframework.data.domain.Pageable;
+import verbly.spring.domain.notification.dto.response.NotificationsResponseDTO;
 
+import java.util.List;
+
+public interface NotificationCommandService {
+    List<NotificationsResponseDTO.NotificationDTO> patchNotifications(Long userId, Pageable pageable);
 }

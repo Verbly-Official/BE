467c3f357fed2f693b0840cba20eb9a763ef3962
tellgeniewish <jadastras2@gmail.com>
2026-02-13 18:01:02 +0900

    [Feat] sms 요청 dto 추가

---
+++ src/main/java/verbly/spring/domain/user/dto/request/SmsRequestDTO.java
@@ -0,0 +1,39 @@
+package verbly.spring.domain.user.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+public class SmsRequestDTO {
+    @Getter
+    @Setter
+    public static class SmsSendDTO {
+        @NotBlank(message = "전화번호는 필수입니다.")
+        @Pattern(
+                regexp = "^[0-9+\\-]{7,20}$",
+                message = "전화번호 형식이 올바르지 않습니다."
+        )
+        @Schema(example = "+8201012345678")
+        private String phoneNumber;
+    }
+
+    @Getter
+    @Setter
+    public class SmsVerifyRequestDTO {
+        @NotBlank(message = "전화번호는 필수입니다.")
+        @Pattern(
+                regexp = "^[0-9+\\-]{7,20}$",
+                message = "전화번호 형식이 올바르지 않습니다."
+        )
+        @Schema(example = "+8201012345678")
+        private String phoneNumber;
+
+        @NotBlank(message = "인증번호는 필수입니다.")
+        @Size(min = 6, max = 6, message = "인증번호는 6자리입니다.")
+        private String code;
+    }
+}

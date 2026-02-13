090d4b4affee7a28dc4715910d05e100db3c8a2b
tellgeniewish <jadastras2@gmail.com>
2026-02-13 18:38:18 +0900

    [Fix] 매개변수 맞추기

--- src/main/java/verbly/spring/domain/user/service/SmsService.java
+++ src/main/java/verbly/spring/domain/user/service/SmsService.java
@@ -2,7 +2,12 @@ package verbly.spring.domain.user.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.nurigo.sdk.message.request.SingleMessageSendingRequest;
+import net.nurigo.sdk.message.response.SingleMessageSentResponse;
+import net.nurigo.sdk.message.service.DefaultMessageService;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+import verbly.spring.domain.user.dto.request.SmsRequestDTO;
 import verbly.spring.global.common.code.ErrorStatus;
 import verbly.spring.global.common.exception.BaseException;
 import verbly.spring.global.common.utils.SmsUtils;
@@ -21,40 +26,40 @@ public class SmsService {
 
     private static final long AUTH_CODE_TTL = 3; // 3분
 
-    public void sendAuthCode(String phoneNumber) {
+    public void sendAuthCode(SmsRequestDTO.SendDTO request) {
         String authCode = smsUtil.generateAuthCode(); // 인증번호 생성
         String messageText = smsUtil.makeAuthMessage(authCode); // 메시지
 
         Message message = new Message();
         message.setFrom(smsProperties.getSender());
-        message.setTo(phoneNumber);
+        message.setTo(request.getPhoneNumber());
         message.setText(messageText);
 
-        SingleMessageSendingRequest request = new SingleMessageSendingRequest(message); // 요청 래핑
+        SingleMessageSendingRequest requestSMS = new SingleMessageSendingRequest(message); // 요청 래핑
 
         // CoolSMS 발송 및 로그
         try {
-            SingleMessageSentResponse response = messageService.sendOne(request);
-            log.info("[SMS] 발송 성공 - to: {}", phoneNumber);
+            SingleMessageSentResponse response = messageService.sendOne(requestSMS);
+            log.info("[SMS] 발송 성공 - to: {}", request.getPhoneNumber());
         } catch (Exception e) {
-            log.error("[SMS] 발송 실패 - to: {}", phoneNumber, e);
+            log.error("[SMS] 발송 실패 - to: {}", request.getPhoneNumber(), e);
             throw new BaseException(ErrorStatus.SMS_SEND_FAILED);
         }
 
         // Redis에 인증번호 저장 (3분 유효)
-        String redisKey = buildKey(phoneNumber);
+        String redisKey = buildKey(request.getPhoneNumber());
         redisTemplate.opsForValue().set(redisKey, authCode, AUTH_CODE_TTL, TimeUnit.MINUTES);
     }
 
-    public void verifyAuthCode(String phoneNumber, String inputCode) {
-        String redisKey = buildKey(phoneNumber);
+    public void verifyAuthCode(SmsRequestDTO.VerifyDTO request) {
+        String redisKey = buildKey(request.getPhoneNumber());
         String storedCode = redisTemplate.opsForValue().get(redisKey);
 
         if (storedCode == null) {
             throw new BaseException(ErrorStatus.SMS_CODE_EXPIRED);
         }
 
-        if (!storedCode.equals(inputCode)) {
+        if (!storedCode.equals(request.getCode())) {
             throw new BaseException(ErrorStatus.SMS_CODE_NOT_MATCH);
         }
 

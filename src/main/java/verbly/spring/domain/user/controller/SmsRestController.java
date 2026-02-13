44f03368c113a9cdb7b277fb2dd39b46e78107bc
tellgeniewish <jadastras2@gmail.com>
2026-02-13 18:24:25 +0900

    [docs] 스웨거 설명 추가

--- src/main/java/verbly/spring/domain/user/controller/SmsRestController.java
+++ src/main/java/verbly/spring/domain/user/controller/SmsRestController.java
@@ -1,5 +1,7 @@
 package verbly.spring.domain.user.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,24 +14,34 @@ import verbly.spring.global.common.code.SuccessStatus;
 @Validated
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "SMS", description = "회원 전화번호 인증 관련 API")
 @RequestMapping("/api/user/phone")
 public class SmsRestController {
     private final SmsService smsService;
 
     @PostMapping("/send")
+    @Operation(
+            summary = "회원 전화번호 수정 시 인증번호 발송 API - JWT AccessToken 인증 필요",
+            description = "JWT 인증된 유저가 입력한 전화번호로 인증번호를 발송합니다. 발송된 인증번호는 3분간 유효합니다.",
+            security = @SecurityRequirement(name = "JWT TOKEN")
+    )
     public ApiResponse<?> sendAuthCode(
-            @RequestParam String phone
+            @Valid @RequestBody SmsRequestDTO.SendDTO request
     ) {
-        smsService.sendAuthCode(phone);
+        smsService.sendAuthCode(request);
         return ApiResponse.onSuccess(SuccessStatus.SMS_SEND_COMPLETED);
     }
 
     @PostMapping("/verify")
+    @Operation(
+            summary = "전화번호 인증번호 검증 API",
+            description = "사용자가 입력한 인증번호를 검증합니다. 인증 성공 시 해당 인증번호는 즉시 만료됩니다.",
+            security = @SecurityRequirement(name = "JWT TOKEN")
+    )
     public ApiResponse<?> verifyAuthCode(
-            @RequestParam String phone,
-            @RequestParam String code
+            @Valid @RequestBody SmsRequestDTO.VerifyDTO request
     ) {
-        smsService.verifyAuthCode(phone, code);
+        smsService.verifyAuthCode(request);
         return ApiResponse.onSuccess(SuccessStatus.SMS_VERIFY_SUCCESS);
     }
 }

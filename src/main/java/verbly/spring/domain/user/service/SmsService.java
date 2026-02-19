d322381564c1ce31a2a53c3074ead5211adad4a4
tellgeniewish <jadastras2@gmail.com>
2026-02-20 00:14:35 +0900

    [Refactor] SMS_TOO_MANY_REQUEST하도록

--- src/main/java/verbly/spring/domain/user/service/SmsService.java
+++ src/main/java/verbly/spring/domain/user/service/SmsService.java
@@ -36,7 +36,7 @@ public class SmsService {
 
         Boolean alreadySent = redisTemplate.hasKey(phoneSendKey);
         if (Boolean.TRUE.equals(alreadySent)) {
-            throw new BaseException(ErrorStatus.SMS_TOO_MANY_ATTEMPTS);
+            throw new BaseException(ErrorStatus.SMS_TOO_MANY_REQUEST);
         }
 
         // 1분 TTL 설정

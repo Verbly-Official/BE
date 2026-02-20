ba30dd18ef01b91bcb0ac49c283a8ad85eae4a34
keypang <bestnest820@naver.com>
2026-02-20 15:29:47 +0900

    [Feat] 연관관계 재설정

--- src/main/java/verbly/spring/domain/notification/entity/Notification.java
+++ src/main/java/verbly/spring/domain/notification/entity/Notification.java
@@ -2,6 +2,8 @@ package verbly.spring.domain.notification.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 import verbly.spring.domain.notification.enums.NotificationType;
 import verbly.spring.domain.user.entity.User;
 import verbly.spring.global.common.entity.BaseEntity;
@@ -18,10 +20,12 @@ public class Notification extends BaseEntity {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "receiver_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private User receiver;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "sender_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private User sender;
 
     private String content;
